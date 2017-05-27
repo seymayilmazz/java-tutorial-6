42b97e19808256bf4591cbd53a600b299e95c114
Şeyma Yılmaz <seyma.yilmaz3@ogr.sakarya.edu.tr>
2017-05-27 18:04:51 +0200

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Şeyma Yılmaz on 27.5.2017.
+ */
+public class Employee {
+
+    String name;
+    int age;
+    int salary;
+
+    public Employee(){
+
+    }
+
+    public Employee(String name, int age, int salary){
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+}
+
+
+
+
+
